961433fa7e9e20b0548414aba6280bb282239251
wladimiroliveira <wladimir2020moraes@outlook.com>
2025-07-18 17:02:46 -0300

    finaliza a configuracao da dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,31 @@
+import CardWrapper from '@/app/ui/dashboard/cards';
+import Revenue from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import { Suspense } from 'react';
+import {
+  CardsSkeleton,
+  RevenueChartSkeleton,
+  LatestInvoicesSkeleton,
+} from '@/app/ui/skeletons';
+
 export default function Page(){
   return (
-    <h1>Dashboard Page</h1>
+    <main>
+      <h1 className={`${lusitana.className} antialiased mb-4 text-xl md:text-2xl`}>Dashboard</h1>
+      <div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-4'>
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
+      </div>
+      <div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <Revenue />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+      </div>
+    </main>
   );
 }
