2450e3b3c37debcc2a4a2a4af7b871b385868351
wladimiroliveira <wladimir2020moraes@outlook.com>
2025-07-18 15:40:09 -0300

    adiciona as imagens

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import Styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -28,7 +29,20 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+            src='/hero-desktop.png'
+            height={1000}
+            width={760}
+            className='hidden md:block'
+            alt='Screenshot from desktop view'
+          />
+          <Image
+            src='/hero-mobile.png'
+            width={560}
+            height={620}
+            className='block md:hidden'
+            alt='Screenshot from mobile view'
+          />
         </div>
       </div>
     </main>
